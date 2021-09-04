9d1ed9c6e1c32e4821257b6a30f82f48bfc81f31
Amy-Code-Analyst <amy.leal@hotmail.com>
2021-09-04 17:53:11 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Automatizacion
+{
+    class Program
+    {
+  
+        static void Main(string[] args)
+        {
+
+            WebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+            driver.Manage().Window.Maximize();//**
+            //
+            IWebElement input = driver.FindElement(By.Id("search_query_top"));
+            input.SendKeys("Vestidos");
+            //
+            IWebElement button = driver.FindElement(By.Name("submit_search"));
+            button.Click();
+            
+        }
+    }
+}
